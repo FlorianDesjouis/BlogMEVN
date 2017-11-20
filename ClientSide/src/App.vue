40da7cf08738fbca1d2f238db3d32997690f686f
FlorianDesjouis <floriandesjouis@supinternet.fr>
2017-11-20 14:19:40 +0000

    initial commit

---
+++ ClientSide/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <main id="app">
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>One</li>
+        <li>Two</li>
+      </ul>
+    </nav>
+    <router-view/>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
